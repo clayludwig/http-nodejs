10395e88feb142a6b530976202afcc19fef825de
clayludwig <87335203+clayludwig@users.noreply.github.com>
2023-02-16 18:13:42 -0500

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,6 +1,26 @@
-import { createServer } from 'http';
+const PORT = 3000;
 
-createServer((req, res) => {
-  res.write('Hello World!');
-  res.end();
-}).listen(process.env.PORT);
+const server = require('http').createServer();
+const io = require('socket.io')(server, {
+    cors: {
+        origin: '*',
+    }
+});
+
+// Listen for incoming connections
+server.listen(PORT, (err) => {
+    if (err) throw err;
+    console.log(`Server started! Listening on port ${PORT}`);
+});
+
+io.on('connection', (client) => {
+    console.log(`Client connected: '${client.id}'`);
+
+    // Send clients SID
+    client.emit('client_id', {
+        client_id: client.id
+    });
+    client.on('disconnect', (data) => {
+        console.log(`Client disconnected: '${client.id}'`);
+    });
+});
